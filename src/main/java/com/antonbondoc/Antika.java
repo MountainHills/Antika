a64b071c9c4d4ce70778cdf3765943d7e14974f5
Anton Bondoc <antonbondoc14@gmail.com>
2024-08-17 23:14:26 +0800

    Create simple CLI application

--- src/main/java/com/antonbondoc/Antika.java
+++ src/main/java/com/antonbondoc/Antika.java
@@ -1,5 +1,12 @@
 package com.antonbondoc;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,7 +14,31 @@ public class Antika {
 
     private static final Logger log = LoggerFactory.getLogger(Antika.class);
 
-    public static void main(String[] args) {
-        log.debug("Hello and welcome to Antika");
+    private static final Option OPTION_DISPLAY = Option.builder("d")
+            .longOpt("display")
+            .hasArg()
+            .argName("message")
+            .desc("Enter a message to display")
+            .build();
+
+    private static Options setupOptions() {
+        return new Options()
+                .addOption(OPTION_DISPLAY);
+    }
+
+    public static void main(String[] args) throws ParseException {
+        log.debug("Starting Antika...");
+        Options options = setupOptions();
+
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        if (cmd.hasOption(OPTION_DISPLAY)) {
+            String message = cmd.getOptionValue(OPTION_DISPLAY);
+            System.out.println("message = " + message);
+        } else {
+            formatter.printHelp("antika", options);
+        }
     }
 }
