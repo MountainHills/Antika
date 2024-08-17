0408d3f8da78106c85c224457e76b65f90aacab8
Anton Bondoc <antonbondoc14@gmail.com>
2024-08-17 21:32:29 +0800

    Setup logger in main class

--- src/main/java/com/antonbondoc/Antika.java
+++ src/main/java/com/antonbondoc/Antika.java
@@ -1,7 +1,13 @@
 package com.antonbondoc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Antika {
+
+    private static final Logger log = LoggerFactory.getLogger(Antika.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello and welcome to Antika");
+        log.info("Hello and welcome to Antika");
     }
 }
